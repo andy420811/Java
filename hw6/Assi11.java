22d2894f69eafc038c6d215a652760e8b0421736
andy420811 <andy420811@gmail.com>
2020-10-19 20:34:55 +0800

    homwork6 on desktop

--- hw6/Assi11.java
+++ hw6/Assi11.java
@@ -3,6 +3,5 @@ class Assi11{
         char[][]ch3 = {{'張','林','古'},{'陳','李'}};
         String [][] str = {{"圓圓","OO"},{"圈圈","洞洞","靈活"}};
         System.out.println(ch3[1][0] + str[0][0] + "漂亮" + "\\" + "陳" + str[1][2] + "快樂^v^");
-        System.out.println(String.format("{0}{1}漂亮\\陳{2}快樂^v^", ch3[1][0],str[0][0],str[1][2]));
     }
 }
---
+++ hw6/Assi22.java
@@ -0,0 +1,11 @@
+public class Assi22 {
+    public static void main(String[] args) {
+        int a =1 ; 
+        int b,c,d;
+        a = a-- - a-- - (b = -(-a/2)) - a - a-- - (c = (a--)/5) + ++a;
+        System.out.println("a=" + a + "\t-(-a/2)=" + b + "\t(a--)/5=" + c);
+        a = 1;
+        a = ++a - (b = --a/2) + ++a + a - --a/(1- --a);
+        System.out.println("a=" + a + "\t--a/2=" + b);
+    }
+}
