fa6a41707e8d9a739c0fc4d680d81bbdba66240f
andy420811 <andy420811@gmail.com>
2020-10-19 20:55:46 +0800

    some modify

--- hw6/Assi22.java
+++ hw6/Assi22.java
@@ -1,7 +1,7 @@
 public class Assi22 {
     public static void main(String[] args) {
-        int a =1 ; 
-        int b,c,d;
+        float  a = 1; 
+        float b,c;
         a = a-- - a-- - (b = -(-a/2)) - a - a-- - (c = (a--)/5) + ++a;
         System.out.println("a=" + a + "\t-(-a/2)=" + b + "\t(a--)/5=" + c);
         a = 1;
